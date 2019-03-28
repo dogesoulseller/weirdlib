54c278558f9521cf80cc89a57ddcdfca60ef8d26
Marcin Czerwonka <mcmarcinoser5@gmail.com>
2019-03-28 16:14:03 +0100

    Add more bit operations

--- include/weirdlib.hpp
+++ include/weirdlib.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <type_traits>
+#include <limits>
 
 namespace wlib
 {
@@ -18,47 +19,47 @@ namespace wlib
 	namespace bop
 	{
 		template<typename T, typename TPos>
-		constexpr inline bool test(const T x, const TPos pos) noexcept {
+		constexpr bool test(const T x, const TPos pos) noexcept {
 			return (x & (1u << pos)) != 0;
 		}
 
 		template<typename T, typename TPos>
-		constexpr inline void set(T* x, const TPos pos) noexcept {
+		void set(T* x, const TPos pos) noexcept {
 			*x |= (1u << pos);
 		}
 
 		template<typename T, typename TPos>
-		constexpr inline T set(const T x, const TPos pos) noexcept {
+		constexpr T set(const T x, const TPos pos) noexcept {
 			return x | (1u << pos);
 		}
 
 		template<typename T, typename TPos>
-		constexpr inline void reset(T* x, const TPos pos) noexcept {
+		void reset(T* x, const TPos pos) noexcept {
 			*x &= ~(1u << pos);
 		}
 
 		template<typename T, typename TPos>
-		constexpr inline T reset(const T x, const TPos pos) noexcept {
+		constexpr T reset(const T x, const TPos pos) noexcept {
 			return x & ~(1u << pos);
 		}
 
 		template<typename T, typename TPos>
-		constexpr inline void toggle(T* x, const TPos pos) noexcept {
+		void toggle(T* x, const TPos pos) noexcept {
 			*x ^= 1u << pos;
 		}
 
 		template<typename T, typename TPos>
-		constexpr inline T toggle(const T x, const TPos pos) noexcept {
+		constexpr T toggle(const T x, const TPos pos) noexcept {
 			return x ^ 1u << pos;
 		}
 
 		template<typename T>
-		constexpr inline T clear_leftmost_set(const T x) noexcept {
+		constexpr T clear_leftmost_set(const T x) noexcept {
 			return x & (x - 1);
 		}
 
 		template<typename T>
-		constexpr uint32_t count_trailing_zeros(T x) noexcept {
+		uint32_t count_trailing_zeros(T x) noexcept {
 			if (x == 0) return (sizeof(T) * 8);
 
 			uint32_t count = 0;
@@ -66,35 +67,36 @@ namespace wlib
 					x = x >> 1;
 					count++;
 			}
+
 			return count;
 		}
 
 		template<typename T>
-		constexpr uint32_t count_leading_zeros(T x) noexcept {
-			if (x == 0) return sizeof(x) * 8;
+		uint32_t count_leading_zeros(T x) noexcept {
+			if (x == 0) return sizeof(T) * 8;
 
-			uint32_t count = 0;
-			while (!(x < 0)) {
-				count++;
+			uint32_t total_bits = sizeof(T) * 8;
+			uint32_t res = 0;
+			while (!(x & (1 << (total_bits - 1)))) {
 				x <<= 1;
+				res++;
 			}
-			return count;
+
+			return res;
 		}
 
 		// Alias for count_trailing_zeros
 		template<typename T>
-		constexpr uint32_t ctz(T&& x) noexcept {
-			return count_trailing_zeros(std::forward<T>(x));
+		uint32_t ctz(T x) noexcept {
+			return count_trailing_zeros(x);
 		}
 
 		// Alias for count_leading_zeros
 		template<typename T>
-		constexpr uint32_t clz(T&& x) noexcept {
-			return count_leading_zeros(std::forward<T>(x));
+		uint32_t clz(T x) noexcept {
+			return count_leading_zeros(x);
 		}
 
 	} // bop
-
-
 
 } // wlib
