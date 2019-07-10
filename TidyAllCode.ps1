ec2ee27f1dc90565625c55c961a9ec6487f9bd9c
Marcin Czerwonka <mcmarcinoser5@gmail.com>
2019-07-10 22:12:13 +0200

    Improve clang-tidy script

--- TidyAllCode.ps1
+++ TidyAllCode.ps1
@@ -6,14 +6,14 @@ Get-ChildItem ./src,./include -Recurse -Include *.cpp,*.hpp |
 		Write-Host ''
 
 		$checks = "bugprone-*,"`
-		+"modernize-*,"`
+		+"modernize-*,-modernize-use-trailing-return-type,-modernize-concat-nested-namespaces,"`
 		+"performance-*,"`
-		+"readability-*,-readability-magic-numbers,-readability-uppercase-literal-suffix,"`
-		+"portability-*,"`
+		+"readability-*,-readability-magic-numbers,-readability-uppercase-literal-suffix,-readability-avoid-const-params-in-decls,"`
+		+"portability-*,-portability-simd-intrinsics,"`
 		+"clang-analyzer-*,"`
 		+"misc-*,"`
 		+"-clang-analyzer-osx*,-clang-analyzer-optin.osx*,-clang-analyzer-optin.mpi*,-clang-analyzer-apiModeling*"
-		clang-tidy $_.FullName --checks=$checks -- -march=native -std=c++17 -Iinclude
+		clang-tidy $_.FullName -p "$PSScriptRoot/build" --checks=$checks -- -march=native -std=c++17 -Iinclude
 
 		Write-Host ''
 		Write-Host 'File done, press any key for next file...'
