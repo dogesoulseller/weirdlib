64905fe669465bdf7cafb30c6142f13d6f68b6a0
Marcin Czerwonka <mcmarcinoser5@gmail.com>
2019-10-10 16:49:11 +0200

    Fix omission of AVX512 strstr version

--- src/stringops/strstr.cpp
+++ src/stringops/strstr.cpp
@@ -19,7 +19,36 @@ namespace wlib::str
 			return str;
 		}
 
-		#if X86_SIMD_LEVEL >= LV_AVX2
+		#if defined(AVX512_BW)
+			// Broadcast first and last character of substring to registers
+			const auto start = _mm512_set1_epi8(needle[0]);
+			const auto end = _mm512_set1_epi8(needle[needleLen-1]);
+
+			// Process 32 bytes of string at once
+			for (size_t i = 0; i < strLen; i+=64) {
+				const auto first = _mm512_loadu_si512(reinterpret_cast<const __m512i*>(str + i));
+				const auto last = _mm512_loadu_si512(reinterpret_cast<const __m512i*>((str + i) + (needleLen - 1)));
+
+				// Calculate mask from hits and misses
+				auto mask = static_cast<uint64_t>(_mm512_movepi8_mask(_mm256_and_si256(_mm256_cmpeq_epi8(start, first), _mm256_cmpeq_epi8(end, last))));
+				while (mask != 0) {
+					// Get lowest set bit
+					const auto pos = bop::bit_scan_reverse(mask);
+
+					// Check for equality between str and needle search position
+					const auto eq0 = str+i+pos+1;
+					const auto eq1 = needle+1;
+					if (std::equal(eq0, eq0+needleLen-2, eq1)) {
+						return str + i + pos;
+					} else {
+						mask = bop::clear_leftmost_set(mask);
+					}
+				}
+			}
+
+			// If nothing was found, return null
+			return nullptr;
+		#elif X86_SIMD_LEVEL >= LV_AVX2
 			// Broadcast first and last character of substring to registers
 			const auto start = _mm256_set1_epi8(needle[0]);
 			const auto end = _mm256_set1_epi8(needle[needleLen-1]);
