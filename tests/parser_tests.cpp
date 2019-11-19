45495ee18f759601fd064e0337f77b17877c3cec
Marcin Czerwonka <mcmarcinoser5@gmail.com>
2019-11-19 11:30:39 +0100

    Improve parser tests

--- tests/parser_tests.cpp
+++ tests/parser_tests.cpp
@@ -23,6 +23,11 @@ TEST(Parse, Comfyg) {
 	auto testBoolF = cfg.GetVal<bool>("ExampleBoolF");
 	auto testString = cfg.GetVal<std::string>("ExampleString");
 
+	auto testFloatLowP = cfg.GetVal<float>("ExampleFloatPlain");
+	auto testFloatHiP  = cfg.GetVal<long double>("ExampleFloatPlain");
+
+	auto testInteger32 = cfg.GetVal<int32_t>("ExampleIntegerPlain");
+
 	// No implicit conversions
 	EXPECT_THROW(cfg.GetVal<bool>("ExampleFloatPlain"), wlib::parse::comfyg_value_get_error);
 	EXPECT_THROW(cfg.GetVal<float>("ExampleFloatNonexistent"), std::out_of_range);
@@ -35,4 +40,9 @@ TEST(Parse, Comfyg) {
 	EXPECT_TRUE(testBoolT);
 	EXPECT_FALSE(testBoolF);
 	EXPECT_EQ(testString, "Lorem ipsum\n Newlines");
+
+	EXPECT_FLOAT_EQ(testFloatLowP, testFloatPlain);
+	EXPECT_FLOAT_EQ(testFloatHiP, testFloatPlain);
+
+	EXPECT_EQ(testInteger32, testIntegerPlain);
 }
